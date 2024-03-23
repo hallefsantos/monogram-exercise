e38a8e43b54f90613412e67ce0fad08646b9c07e
Hallef Santos <hallefsantos@gmail.com>
2024-03-23 12:16:10 -0300

    feat: review hero (basics)

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,17 +1,23 @@
-<section class="pt-[153px]">
-  <div class="container mx-auto">
-    <h1 class="text-4xl font-black">jamstack
-      <span class="bg-gradient-to-r from-[#fdbf85] via-[#f38e80] to-[#ca77a3] bg-clip-text text-transparent">
-        101
-    </span>
-    </h1>
-    <div class="mt-11 max-w-[550px] w-full ml-[52px]">
-      <p class="font-light text-sm text-gray-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Arcu augue massa, tincidunt proin nunc viverra tristique tempor, ipsum. At lectus vel pretium tempor ut dui vivamus sit.</p>
-    </div>
-
-    <div>
-      <img src="shape-hero-left.png" alt="">
-      <img src="shape-hero-right.png" alt="">
+<section class="pt-[153px] h-screen">
+  <div class="max-w-[1320px] mx-auto">
+    <div class="flex">
+      <div>
+        <h1 class="text-4xl font-black">jamstack
+          <span class="bg-gradient-to-r from-[#fdbf85] via-[#f38e80] to-[#ca77a3] bg-clip-text text-transparent">
+            101
+        </span>
+        </h1>
+        <div class="mt-11 max-w-[550px] w-full ml-[52px]">
+          <p class="font-light text-sm text-gray-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Arcu augue massa, tincidunt proin nunc viverra tristique tempor, ipsum. At lectus vel pretium tempor ut dui vivamus sit.</p>
+        </div>
+        <img class="mt-[87px] ml-[256px]" src="/mouse-line.svg" alt="Mouse line">
+      </div>
+      <div class="relative flex-grow h-[964px]">
+        <img class="absolute top-[171px] right-[340px]" src="shape-hero-left.png" alt="">
+        <img class="absolute top-0 right-4" src="shape-hero-right.png" alt="">
+        <span class="absolute bottom-0 right-[176px] block w-[280px] h-[479px] bg-white/50 rounded-[68px] backdrop-blur-[60px]"></span>
+        <span class="absolute bottom-0 -right-[144px] block w-[280px] h-[662px] bg-white/50 rounded-[68px] backdrop-blur-[60px]"></span>
+      </div>
     </div>
   </div>
 </section>
