6eb4e6518df9784cbb6e88d7f3519937c3030f44
Hallef Santos <hallefsantos@gmail.com>
2024-03-22 21:43:55 -0300

    chore: add font Inter / add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,38 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    colors: {
+      'gray': {
+        100: '#E8E8E8',
+        200: '#C7C7C7',
+        300: '#969696',
+        400: '#757575',
+        500: '#444444',
+        600: '#414141',
+        700: '#3E3D3C',
+        800: '#1A1A1A',
+        900: '#151515',
+      },
+      'blue': {
+        400: '#44DBFF',
+        500: '#0165B1',
+      },
+      'green': {
+        100: '#EFFFE2',
+        500: '#97B1B2',
+      },
+      black: '#000000',
+      orange: '#FEC05C',
+      red: '#D12510',
+      pink: '#FF069C',
+      white: '#FFFFFF',
+    },
+    fontFamily: {
+      'sans': ['Inter', ...defaultTheme.fontFamily.sans],
+    },
   },
   plugins: [],
 }
