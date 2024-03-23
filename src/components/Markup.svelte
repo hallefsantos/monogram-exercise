cedaad471c41de3eb7c80598401e9f9180d162e5
Hallef Santos <hallefsantos@gmail.com>
2024-03-23 11:43:32 -0300

    feat: add markup (basic)

---
+++ src/components/Markup.svelte
@@ -0,0 +1,8 @@
+<section class="pt-[186px] pb-20">
+  <div class="container mx-auto px-5">
+    <div class="max-w-[480px] w-full">
+      <h2 class="text-4xl font-black">markup</h2>
+      <p class="mt-[69px] text-md">When ready for deployment, a static-site generator such as Astro or Next.js is used to compile the website. The end result is a collection of pre-rendered HTML pages that can be delivered lightning-fast over a CDN like Vercel’s Edge Network.</p>
+    </div>
+  </div>
+</section>
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as Footer } from './Footer.svelte'
 export { default as Hero } from './Hero.svelte'
+export { default as Markup } from './Markup.svelte'
