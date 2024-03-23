b20878d8e908714bc0313f798b116d8f042d89ad
Hallef Santos <hallefsantos@gmail.com>
2024-03-22 21:44:33 -0300

    feat: add footer component

---
+++ src/components/Footer.svelte
@@ -0,0 +1,6 @@
+<footer class="pt-[245px] pb-[156px] bg-gray-900">
+  <div class="container px-5 text-center">
+    <p class="text-2xl text-white">This is a design test by <a href="https://github.com/hallefsantos" target="_blank">@hallefsantos</a> for Monogram.</p>
+    <span class="inline-block text-pink text-5xl mt-[30px]"> ❤️ </span> <!-- Fix font-size -->
+  </div>
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,9 @@
+<script lang="ts">
+  import Footer from '$components/Footer.svelte'
+</script>
+
 <svelte:head>
   <title>Front-end Development Exercise III</title>
 </svelte:head>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Footer />
