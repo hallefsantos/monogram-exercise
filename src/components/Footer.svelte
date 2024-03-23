0aeb994b217845fcc325816d19251099e85d2bc3
Hallef Santos <hallefsantos@gmail.com>
2024-03-23 11:43:03 -0300

    fix: footer margin

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,5 +1,5 @@
 <footer class="pt-[245px] pb-[156px] bg-gray-900">
-  <div class="container px-5 text-center">
+  <div class="container mx-auto px-5 text-center">
     <p class="text-md text-white">This is a design test by <a href="https://github.com/hallefsantos" target="_blank">@hallefsantos</a> for Monogram.</p>
     <span class="inline-block text-pink text-xl mt-[30px]"> ❤️ </span> <!-- Fix font-size -->
   </div>
