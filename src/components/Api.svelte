4dffe960d0d4395e392d7e8519946d72029b9be9
Hallef Santos <hallefsantos@gmail.com>
2024-03-25 18:30:21 -0300

    feat: api (mobile)

--- src/components/Api.svelte
+++ src/components/Api.svelte
@@ -1,39 +1,39 @@
-<section class="relative pt-[176px] bg-gray-900">
-  <div class="grid grid-cols-4 max-w-[1280px] w-full mx-auto">
-    <div class="col-start-4 flex justify-center" data-aos="fade-up">
+<section class="relative pt-[275px] bg-gray-900">
+  <div class="grid grid-cols-3 xl:grid-cols-4 gap-6 xl:gap-0 max-w-[1280px] w-full mx-auto px-20">
+    <div class="col-start-3 xl:col-start-4 flex justify-center" data-aos="fade-up">
       <img class="drop-shadow-path" src="/nextjs.svg" alt="Nextjs logo" width="124" height="75">
     </div>
-    <div class="row-start-2 flex justify-center mt-[182px]" data-aos="fade-up">
+    <div class="row-start-2 flex justify-center xl:mt-[32px]" data-aos="fade-up">
       <img class="drop-shadow-path" src="/react.svg" alt="React logo" width="200" height="55">
     </div>
-    <div class="row-start-3 col-start-2 flex justify-center mt-[72px]" data-aos="fade-up">
+    <div class="row-start-3 col-start-2 flex justify-center xl:mt-[52px]" data-aos="fade-up">
       <img class="drop-shadow-path" src="/nextjs.svg" alt="Nextjs logo" width="124" height="75">
     </div>
-    <div class="row-start-4 col-start-4 flex justify-center mt-3" data-aos="fade-up">
+    <div class="row-start-4 col-start-3 xl:col-start-4 flex justify-center mt-3" data-aos="fade-up">
       <img class="drop-shadow-path" src="/react.svg" alt="React logo" width="200" height="55">
     </div>
-    <div class="row-start-5 flex justify-center mt-[142px]" data-aos="fade-up">
+    <div class="row-start-5 flex justify-center mt-[42px]" data-aos="fade-up">
       <img class="drop-shadow-path opacity-40" src="/svelte.svg" alt="Svelte logo" width="220" height="53">
     </div>
-    <div class="row-start-5 col-start-3 flex justify-center mt-[42px]" data-aos="fade-up">
-      <img class="drop-shadow-path" src="/vuejs.svg" alt="Vuejs logo" width="180" height="65">
+    <div class="row-start-5 col-start-3 flex justify-center mt-[22px]" data-aos="fade-up">
+      <img class="drop-shadow-path opacity-60 xl:opacity-100" src="/vuejs.svg" alt="Vuejs logo" width="180" height="65">
     </div>
-    <div class="row-start-6 col-start-2 flex justify-center mt-[173px]" data-aos="fade-up">
+    <div class="row-start-6 col-start-2 flex justify-center mt-[73px]" data-aos="fade-up">
       <img class="drop-shadow-path opacity-10" src="/react.svg" alt="React logo" width="200" height="55">
     </div>
-    <div class="row-start-7 col-start-4 flex justify-center mt-[105px]" data-aos="fade-up">
+    <div class="row-start-7 xl:col-start-4 flex justify-center mt-[35px]" data-aos="fade-up">
       <img class="drop-shadow-path opacity-10" src="/svelte.svg" alt="Svelte logo" width="220" height="53">
     </div>
   </div>
 
-  <div class="max-w-[770px] w-full mx-auto px-5 text-center">
-    <h2 class="text-4xl font-black text-white">api</h2>
-    <p class="mt-[63px] text-md text-white">At build time, a Jamstack website uses data from one or more APIs to generate markup. These APIs can be a headless CMS like Prismic, a database like Supabase, or even a local JSON file!</p>
+  <div class="max-w-[770px] mt-10 ml:mt-0 w-full mx-auto px-5 text-center">
+    <h2 class="text-[50px] sm:text-4xl font-black text-white">api</h2>
+    <p class="mt-5 xl:mt-[63px] text-sm xl:text-md text-white">At build time, a Jamstack website uses data from one or more APIs to generate markup. These APIs can be a headless CMS like Prismic, a database like Supabase, or even a local JSON file!</p>
   </div>
 
   <!-- globe -->
-  <div class="relative before:absolute before:left-0 before:top-0 before:w-full before:h-2/6 before:bg-gray-900 bg-white">
+  <div class="relative before:absolute before:left-0 before:top-0 before:w-full before:h-2/6 before:bg-gray-900 mt-16 xl:mt-[170px] px-5 bg-white">
     <img src="/squiggly.svg" class="absolute top-[31%] left-0 w-full">
-    <img class="relative z-10 mx-auto mt-[170px]" src="/3d-circle.png" alt="Globe">
+    <img class="relative z-10 mx-auto" src="/3d-circle.png" alt="Globe">
   </div>
 </section>
