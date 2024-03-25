e31fa6bb77aeb1ba837bd46e8a11946ff14fcdc7
Hallef Santos <hallefsantos@gmail.com>
2024-03-25 17:45:04 -0300

    feat: hero (mobile)

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,19 +1,19 @@
-<section class="relative pt-[153px] bg-white">
+<section class="relative pt-14 sm:pt-[153px] bg-white">
   <div class="max-w-[1320px] mx-auto px-5">
     <div class="flex">
       <div>
-        <h1 class="text-4xl font-black">
+        <h1 class="text-[50px] sm:text-4xl font-black">
           jamstack
           <span class="bg-gradient-to-r from-[#fdbf85] via-[#f38e80] to-[#ca77a3] bg-clip-text text-transparent">
             101
         </span>
         </h1>
-        <div class="mt-11 max-w-[550px] w-full ml-[52px]">
-          <p class="font-light text-sm text-gray-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Arcu augue massa, tincidunt proin nunc viverra tristique tempor, ipsum. At lectus vel pretium tempor ut dui vivamus sit.</p>
+        <div class="mt-5 sm:mt-11 max-w-[550px] w-full sm:ml-[52px]">
+          <p class="font-light text-[16px] sm:text-sm text-gray-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Arcu augue massa, tincidunt proin nunc viverra tristique tempor, ipsum. At lectus vel pretium tempor ut dui vivamus sit.</p>
         </div>
-        <img class="mt-[87px] ml-[256px]" src="/mouse-line.svg" alt="Mouse line">
+        <img class="mt-[87px] ml-[256px] hidden md:block" src="/mouse-line.svg" alt="Mouse line">
       </div>
-      <div class="relative flex-grow h-[964px]">
+      <div class="relative flex-grow h-[964px] hidden xl:block">
         <img class="absolute top-[171px] right-[340px] animate-updown" src="/shape-hero-left.png" alt="" data-aos="fade-up" data-aos-delay="300">
         <img class="absolute top-0 right-4 animate-updown animation-delay-1000" src="/shape-hero-right.png" alt="" data-aos="fade-up">
         <span class="absolute bottom-0 right-[176px] block w-[280px] h-[479px] bg-white/50 rounded-[68px] backdrop-blur-[60px]" data-aos="fade-up" data-aos-delay="600"></span>
