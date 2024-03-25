9f351d2e477dc93d9d531fa7a69e3fc749b7c9ba
Hallef Santos <hallefsantos@gmail.com>
2024-03-25 19:19:55 -0300

    feat: review markup (mobile)

--- src/components/Markup.svelte
+++ src/components/Markup.svelte
@@ -1,11 +1,11 @@
-<section class="relative z-20 pt-[186px] bg-[url('/markup-blocks.svg')] bg-repeat-x bg-bottom">
+<section class="relative z-20 pt-10 xl:pt-[186px] pb-64 bg-[url('/markup-blocks.svg')] bg-repeat-x bg-bottom">
   <div class="max-w-[1320px] mx-auto px-5">
     <div class="flex">
       <div class="max-w-[480px] w-full">
-        <h1 class="text-4xl font-black" data-aos="fade-right">markup</h1>
-        <p class="mt-[69px] font-light text-md" data-aos="fade-right">When ready for deployment, a static-site generator such as Astro or Next.js is used to compile the website. The end result is a collection of pre-rendered HTML pages that can be delivered lightning-fast over a CDN like Vercel’s Edge Network.</p>
+        <h1 class="text-[50px] sm:text-4xl font-black" data-aos="fade-right">markup</h1>
+        <p class="mt-5 xl:mt-[69px] font-light text-[16px] xl:text-md" data-aos="fade-right">When ready for deployment, a static-site generator such as Astro or Next.js is used to compile the website. The end result is a collection of pre-rendered HTML pages that can be delivered lightning-fast over a CDN like Vercel’s Edge Network.</p>
       </div>
-      <div class="relative flex-grow mt-[26px] mb-[269px] h-[964px]">
+      <div class="relative hidden xl:block flex-grow mt-[26px] mb-[269px] h-[964px]">
         <img class="absolute top-[171px] right-[340px] animate-updown" src="/shape-markup-left.jpg" alt="" data-aos="fade-up" data-aos-delay="300" width="280" height="629">
         <img class="absolute top-0 right-4 animate-updown animation-delay-1000" src="/shape-markup-right.jpg" alt="" data-aos="fade-up">
         <span class="absolute bottom-0 right-[176px] block w-[280px] h-[479px] bg-white/50 rounded-[68px] backdrop-blur-[60px]" data-aos="fade-up" data-aos-delay="600"></span>
