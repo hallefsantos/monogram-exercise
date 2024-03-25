443536990c2667c84bdc5a4af002575f94f1e552
Hallef Santos <hallefsantos@gmail.com>
2024-03-25 19:52:21 -0300

    feat: review AuditScores (mobile)

--- src/components/AuditScores/AuditScores.svelte
+++ src/components/AuditScores/AuditScores.svelte
@@ -1,20 +1,20 @@
 <script>
 	import { Score } from '.'
 </script>
-<section class="relative bg-gray-900">
-  <div class="relative z-20 -top-20 before:absolute before:inset-0 before:bg-[linear-gradient(25.39deg,_rgba(255,_255,_255,_0.024)_18.54%,_rgba(123,_245,_93,_0.108)_62.25%)] before:backdrop-blur-2xl before:scale-x-[-1] max-w-[1204px] w-full h-[1328px] pt-[120px] px-[77px] pb-[217px] mx-auto rounded-[68px] border-2 border-[#7BF55D]/10 overflow-hidden">
-    <div class="relative z-10">
-      <h2 class="text-green-100 font-black text-center text-3xl">top audit scores</h2>
-      <div class="flex justify-between items-center mt-[96px]">
+<section class="relative bg-gray-900 px-4">
+  <div class="relative z-20 -top-20 before:absolute before:inset-0 before:bg-[linear-gradient(25.39deg,_rgba(255,_255,_255,_0.024)_18.54%,_rgba(123,_245,_93,_0.108)_62.25%)] before:backdrop-blur-2xl before:scale-x-[-1] max-w-[1204px] w-full xl:h-[1328px] py-10 xl:pt-[120px] px-4 xl:px-[77px] xl:pb-[217px] mx-auto rounded-3xl xl:rounded-[68px] border-2 border-[#7BF55D]/10 overflow-hidden">
+    <div class="relative z-10 flex flex-col items-center">
+      <h2 class="text-green-100 font-black text-center text-[32px] xl:text-3xl">top audit scores</h2>
+      <div class="grid grid-cols-2 sm:grid-cols-4 gap-4 xl:flex xl:justify-between xl:items-center xl:w-full mt-10 xl:mt-[96px]">
         <Score />
         <Score />
         <Score />
         <Score />
       </div>
-      <div class="max-w-[756px] w-full mt-32 mx-auto space-y-16 [&>p]:text-white [&>p]:text-lg">
-        <p>Search engines, business stakeholders, and end-users gauge a website’s value using metric tools like Google Lighthouse, which measures site performance, SEO, and accessibility.</p>
-        <p>Performance is one of the most difficult scores to get to 100. This is especially true in a time when users demand JavaScript-heavy, highly-interactive web experiences.</p>
-        <p>A Jamstack website, delivering SEO-friendly, lean & clean HTML in milliseconds, promises higher search-engine ranking and a more positive, responsive end-user experience.</p>
+      <div class="xl:max-w-[756px] w-full mt-10 xl:mt-32 mx-auto space-y-5 xl:space-y-16 [&>p]:text-white xl:[&>p]:text-lg opacity-70">
+        <p class="mix-blend-color-dodge">Search engines, business stakeholders, and end-users gauge a website’s value using metric tools like Google Lighthouse, which measures site performance, SEO, and accessibility.</p>
+        <p class="mix-blend-color-dodge">Performance is one of the most difficult scores to get to 100. This is especially true in a time when users demand JavaScript-heavy, highly-interactive web experiences.</p>
+        <p class="mix-blend-color-dodge">A Jamstack website, delivering SEO-friendly, lean & clean HTML in milliseconds, promises higher search-engine ranking and a more positive, responsive end-user experience.</p>
       </div>
     </div>
   </div>
--- src/components/AuditScores/Score.svelte
+++ src/components/AuditScores/Score.svelte
@@ -1,3 +1,3 @@
-<div class="grid place-items-center max-w-[212px] w-full aspect-square border-2 border-green-100 rounded-full">
-  <span class="text-green-100 font-bold text-2xl [text-shadow:_0px_0px_12px_#53FB2A,_0px_0px_42px_#E9E11F,_0px_0px_61px_#53FB2A]">100</span>
+<div class="grid place-items-center xl:max-w-[212px] w-[112px] xl:w-full mx-auto aspect-square border-2 border-green-100 rounded-full">
+  <span class="text-green-100 font-bold text-lg xl:text-2xl [text-shadow:_0px_0px_12px_#53FB2A,_0px_0px_42px_#E9E11F,_0px_0px_61px_#53FB2A]">100</span>
 </div>
