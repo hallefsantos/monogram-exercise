497d72a0f17b26cda6a02d25829dc14e14df51f3
Hallef Santos <hallefsantos@gmail.com>
2024-03-25 18:33:05 -0300

    feat: remove these for now

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,21 +17,21 @@
 </svelte:head>
 
 <!-- Background -->
-<div class="fixed z-10 left-1/2 -translate-x-1/2 inset-0 max-w-[1280px] w-full h-full flex justify-between opacity-20 pointer-events-none">
+<div class="fixed z-10 left-1/2 -translate-x-1/2 inset-0 max-w-[1280px] w-full h-full flex justify-between px-16 xl:px-0 opacity-20 pointer-events-none">
   <span class="line inline-block w-[1px] bg-repeat-y bg-[length:1px_8px]"></span>
   <span class="line inline-block w-[1px] bg-repeat-y bg-[length:1px_8px]"></span>
   <span class="line inline-block w-[1px] bg-repeat-y bg-[length:1px_8px]"></span>
   <span class="line inline-block w-[1px] bg-repeat-y bg-[length:1px_8px]"></span>
-  <span class="line inline-block w-[1px] bg-repeat-y bg-[length:1px_8px]"></span>
+  <span class="line hidden xl:inline-block w-[1px] bg-repeat-y bg-[length:1px_8px]"></span>
 </div>
 
 
 <Hero />
 <Javascript />
 <Api />
-<Markup />
+<!-- <Markup />
 <AuditScores />
-<Footer />
+<Footer /> -->
 
 <style>
   .line {
