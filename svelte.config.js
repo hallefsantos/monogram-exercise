9b3bdbcc3ccb1844f252a39df9bebb5a5d5350d3
Hallef Santos <hallefsantos@gmail.com>
2024-03-27 16:26:52 -0300

    review ssg options

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import adapter from '@sveltejs/adapter-static'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -17,14 +17,8 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		})
+      fallback: '200.html' // may differ from host to host
+    })
 	}
 };
 
