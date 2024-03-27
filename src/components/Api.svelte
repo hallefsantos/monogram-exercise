f0758b4697211d938cfbbb640ab0b6bcd67b4a3a
Hallef Santos <hallefsantos@gmail.com>
2024-03-27 07:21:34 -0300

    add lazy loading

--- src/components/Api.svelte
+++ src/components/Api.svelte
@@ -6,28 +6,28 @@
 <section class="relative pt-[275px] bg-gray-900">
   <div class="grid grid-cols-3 xl:grid-cols-4 gap-6 xl:gap-0 max-w-[1280px] w-full mx-auto px-20 xl:px-0">
     <div class="col-start-3 xl:col-start-4 flex justify-center" data-aos="fade-up">
-      <img class="xl:drop-shadow-path" src="/nextjs.svg" alt="Nextjs logo" width="124" height="75">
+      <img loading="lazy" class="xl:drop-shadow-path" src="/nextjs.svg" alt="Nextjs logo" width="124" height="75">
     </div>
     <div class="row-start-2 flex justify-center xl:mt-[32px]" data-aos="fade-up">
-      <img class="xl:drop-shadow-path" src="/react.svg" alt="React logo" width="200" height="55">
+      <img loading="lazy" class="xl:drop-shadow-path" src="/react.svg" alt="React logo" width="200" height="55">
     </div>
     <div class="row-start-3 col-start-2 flex justify-center xl:mt-[52px]" data-aos="fade-up">
-      <img class="xl:drop-shadow-path" src="/nextjs.svg" alt="Nextjs logo" width="124" height="75">
+      <img loading="lazy" class="xl:drop-shadow-path" src="/nextjs.svg" alt="Nextjs logo" width="124" height="75">
     </div>
     <div class="row-start-4 col-start-3 xl:col-start-4 flex justify-center mt-3" data-aos="fade-up">
-      <img class="xl:drop-shadow-path" src="/react.svg" alt="React logo" width="200" height="55">
+      <img loading="lazy" class="xl:drop-shadow-path" src="/react.svg" alt="React logo" width="200" height="55">
     </div>
     <div class="row-start-5 flex justify-center mt-[42px]" data-aos="fade-up">
-      <img class="opacity-40 xl:drop-shadow-path" src="/svelte.svg" alt="Svelte logo" width="220" height="53">
+      <img loading="lazy" class="opacity-40 xl:drop-shadow-path" src="/svelte.svg" alt="Svelte logo" width="220" height="53">
     </div>
     <div class="row-start-5 col-start-3 flex justify-center mt-[22px]" data-aos="fade-up">
-      <img class="opacity-60 xl:opacity-100 xl:drop-shadow-path" src="/vuejs.svg" alt="Vuejs logo" width="180" height="65">
+      <img loading="lazy" class="opacity-60 xl:opacity-100 xl:drop-shadow-path" src="/vuejs.svg" alt="Vuejs logo" width="180" height="65">
     </div>
     <div class="row-start-6 col-start-2 flex justify-center mt-[73px]" data-aos="fade-up">
-      <img class="opacity-10 xl:drop-shadow-path" src="/react.svg" alt="React logo" width="200" height="55">
+      <img loading="lazy" class="opacity-10 xl:drop-shadow-path" src="/react.svg" alt="React logo" width="200" height="55">
     </div>
     <div class="row-start-7 xl:col-start-4 flex justify-center mt-[35px]" data-aos="fade-up">
-      <img class="opacity-10 xl:drop-shadow-path" src="/svelte.svg" alt="Svelte logo" width="220" height="53">
+      <img loading="lazy" class="opacity-10 xl:drop-shadow-path" src="/svelte.svg" alt="Svelte logo" width="220" height="53">
     </div>
   </div>
 
--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -11,11 +11,11 @@
         <div class="mt-5 sm:mt-11 max-w-[550px] w-full sm:ml-[52px]">
           <p class="font-light text-[16px] sm:text-sm text-gray-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Arcu augue massa, tincidunt proin nunc viverra tristique tempor, ipsum. At lectus vel pretium tempor ut dui vivamus sit.</p>
         </div>
-        <img class="mt-[87px] ml-[256px] hidden md:block" src="/mouse-line.svg" alt="Mouse line">
+        <img loading="lazy" class="mt-[87px] ml-[256px] hidden md:block" src="/mouse-line.svg" alt="Mouse line">
       </div>
       <div class="relative flex-grow h-[964px] hidden xl:block">
-        <img class="absolute top-[171px] right-[340px] animate-updown" src="/shape-hero-left.webp" alt="" data-aos="fade-up" data-aos-delay="300">
-        <img class="absolute top-0 right-4 animate-updown animation-delay-1000" src="/shape-hero-right.webp" alt="" data-aos="fade-up">
+        <img loading="lazy" class="absolute top-[171px] right-[340px] animate-updown" src="/shape-hero-left.webp" alt="" data-aos="fade-up" data-aos-delay="300">
+        <img loading="lazy" class="absolute top-0 right-4 animate-updown animation-delay-1000" src="/shape-hero-right.webp" alt="" data-aos="fade-up">
         <span class="absolute bottom-0 right-[176px] block w-[280px] h-[479px] bg-white/50 rounded-[68px] backdrop-blur-[60px]" data-aos="fade-up" data-aos-delay="600"></span>
         <span class="absolute bottom-0 -right-[144px] block w-[280px] h-[662px] bg-white/50 rounded-[68px] backdrop-blur-[60px]" data-aos="fade-up" data-aos-delay="900"></span>
       </div>
