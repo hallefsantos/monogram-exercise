a4682b6408c2266ad12a91dd7e8e2c3041d513f4
Hallef Santos <hallefsantos@gmail.com>
2024-03-27 21:31:49 -0300

    feat: add missing block in markup section

--- src/components/Markup.svelte
+++ src/components/Markup.svelte
@@ -1,11 +1,16 @@
 <section class="relative z-20 pt-10 xl:pt-[186px] pb-64 bg-[url('/markup-blocks.svg')] bg-repeat-x bg-bottom">
-  <div class="max-w-[1320px] mx-auto px-5">
+  <div class="relative max-w-[1320px] mx-auto px-5">
+    <img
+      class="absolute right-10 xl:-left-10 bottom-0 hidden lg:block w-96"
+      src="/markup-blocks-details.svg"
+      alt="Blocks details"
+    >
     <div class="flex">
-      <div class="max-w-[480px] w-full">
-        <h1 class="text-[50px] sm:text-4xl font-black" data-aos="fade-right">markup</h1>
+      <div class="lg:max-w-[480px] w-full text-center lg:text-left">
+        <h1 class="text-[40px] sm:text-2xl lg:text-4xl font-black" data-aos="fade-right">markup</h1>
         <p class="mt-5 xl:mt-[69px] font-light text-[16px] xl:text-md" data-aos="fade-right">When ready for deployment, a static-site generator such as Astro or Next.js is used to compile the website. The end result is a collection of pre-rendered HTML pages that can be delivered lightning-fast over a CDN like Vercel’s Edge Network.</p>
       </div>
-      <div class="relative hidden xl:block flex-grow mt-[26px] mb-[269px] h-[964px]">
+      <div class="relative hidden xl:block flex-grow mt-[26px] h-[964px]">
         <img loading="lazy" class="absolute top-[171px] right-[340px] animate-updown" src="/shape-markup-left.webp" alt="" data-aos="fade-up" data-aos-delay="300" width="280" height="629">
         <img loading="lazy" class="absolute top-0 right-4 animate-updown animation-delay-1000" src="/shape-markup-right.webp" alt="" data-aos="fade-up">
         <span class="absolute bottom-0 right-[176px] block w-[280px] h-[479px] bg-white/50 rounded-[68px] backdrop-blur-[60px]" data-aos="fade-up" data-aos-delay="600"></span>
